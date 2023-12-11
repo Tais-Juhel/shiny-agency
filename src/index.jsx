1cff74062f02aa7ef6ced3c65e9d46ced909fcb0
Tais-Juhel <juhel.tais@ynov.vom>
2023-12-11 19:20:01 +0100

    prettier, eslint and router

---
+++ src/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Survey from './pages/Survey'
+import Header from './components/Header'
+import ErrorPage from './components/ErrorPage'
+import Results from './pages/Results'
+import Freelances from './pages/Freelances'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/survey/:questionNumber" element={<Survey />} />
+        <Route path="/results" element={<Results />} />
+        <Route path="/freelances" element={<Freelances />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  </React.StrictMode>,
+)
---
+++ src/pages/Survey/index.jsx
@@ -0,0 +1,22 @@
+import { useParams, Link } from 'react-router-dom'
+
+function Survey() {
+  const { questionNumber } = useParams()
+  const questionNumberInt = parseInt(questionNumber)
+  const prevQuestionNumber = questionNumberInt === 1 ? 1 : questionNumberInt - 1
+  const nextQuestionNumber = questionNumberInt + 1
+
+  return (
+    <div>
+      <h1>Questionnaire</h1>
+      <Link to={`/survey/${prevQuestionNumber}`}>Précédent</Link>
+      {questionNumberInt === 10 ? (
+        <Link to="/results">Résultats</Link>
+      ) : (
+        <Link to={`/survey/${nextQuestionNumber}`}>Suivant</Link>
+      )}
+    </div>
+  )
+}
+
+export default Survey
